7d76f596fb654b4720e051597cb1e7af547841f0
David Smith <david.smith@weaveability.com>
2020-01-22 09:35:57 +0000

    server scaffolding

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
     "no-useless-return": "error",
     "no-var": "error",
     "one-var": ["error", "never"],
-    "prefer-arrow-callback": "error",
+    // "prefer-arrow-callback": "error",
     strict: "error",
     "symbol-description": "error",
     yoda: ["error", "never", { exceptRange: true }]
